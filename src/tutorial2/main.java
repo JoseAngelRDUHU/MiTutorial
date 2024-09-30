b883e529a4a06e35c692459ec4f7ed4fecf85eb9
JoseAngelRD <JoseAngelRD@DESKTOP-ELO7BIL>
2024-09-30 19:08:13 +0200

    Commit Branch 2 Paso 10 Discontinued

--- src/tutorial2/main.java
+++ src/tutorial2/main.java
@@ -7,6 +7,7 @@ public class main {
 		System.out.println("hola mundo xd"); 
 		System.out.println("Este es el cambio del branch 1"); 
 		System.out.println("Este es mi branch 2, del paso 5, y no acabará en la rama master");
+		System.out.println("ESTA RAMA NO SIRVE, Y ESTE ES SU ULTIMO CAMBIO");
 	}
 
 }
