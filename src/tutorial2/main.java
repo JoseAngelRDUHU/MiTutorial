109e377969f761621b2669192470d30ff03f78a3
JoseAngelRD <JoseAngelRD@DESKTOP-ELO7BIL>
2024-09-30 19:00:57 +0200

    Mi commit 2 - Paso 7

--- src/tutorial2/main.java
+++ src/tutorial2/main.java
@@ -6,6 +6,7 @@ public class main {
 		
 		System.out.println("hola mundo xd"); 
 		System.out.println("Este es el cambio del branch 1"); 
+		System.out.println("Este es mi cambio del branch 3, y me dirijo al paso 7");
 	}
 
 }
